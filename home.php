7d6ea4147e06962208653b81a17b3280444a7048
Ravindusachethana <87492319+Ravindusachethana@users.noreply.github.com>
2021-07-15 22:25:14 +0530

    main content created

---
+++ home.php
@@ -0,0 +1,209 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Learn &mdash; Free Website Template, Free HTML5 Template by freehtml5.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="freehtml5.co" />
+
+	<!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FreeHTML5.co
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,700,800" rel="stylesheet">
+	
+	<link rel="stylesheet" href="./css/otherStyles.css">
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="./css/">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="top">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-1">
+						<div id="fh5co-logo"><a href="index.html">LOGO<span>.</span></a></div>
+					</div>
+					<div class="col-xs-11 text-right">
+						<p class="num">Call: +01 123 456 7890</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="top-menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-9">
+						<h2 class="heading">Welcome to the Distance learning portal of MATRIX</h2>
+					</div>
+					<div class="col-xs-3 text-right menu-1">
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="index.html">LMS</a></li>
+							<li><a href="pricing.html">About us</a></li>
+							<!-- <li class="has-dropdown active">
+								<a href="blog.html">Blog</a>
+								<ul class="dropdown">
+									<li><a href="#">Web Design</a></li>
+									<li><a href="#">eCommerce</a></li>
+									<li><a href="#">Branding</a></li>
+									<li><a href="#">API</a></li>
+								</ul>
+							</li> -->
+							<!-- <li><a href="contact.html">Contact</a></li> -->
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+	<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(./images/Elearning.jpg);" data-stellar-background-ratio="0.5">
+	</header>
+	</div>
+	<div class="container">
+        <div class="row align-items-center">
+            <div class="col-xs-7 mb-3 text-center">
+              <h1 data-aos="fade-up" data-aos-delay="100" class="aos-init aos-animate text-white">Plan your Road to Success. </h1>
+              <p class=" text-white mb-4 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">Student centric education to international standards.</p>
+            </div>    
+
+            <div class="col-sm-4 ml-auto aos-init aos-animate formContent" data-aos="fade-up" data-aos-delay="500">
+              <form action="" method="post" class="form-box">
+                <h3 class="h4 text-black mb-4 pt-5">Login</h3>
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Email Addresss">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" placeholder="Password">
+                </div>
+                <div class="form-group mb-4">
+                  <input type="password" class="form-control" placeholder="Re-type Password">
+                </div>
+                <div class="form-group pb-4">
+                  <input type="submit" class="btn btn-primary btn-pill" value="Login">
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="row align-items-center">
+            <div class="col-sm-12 mb-4 pt-lg-5">
+              <h1 data-aos="fade-up" data-aos-delay="100" class="text-white aos-init aos-animate">Why study from a distance ?</h1>
+              <p class="mb-4 aos-init aos-animate text-white" data-aos="fade-up" data-aos-delay="200">Studying a degree through distance learning gives you a number of benefits: You can fit your learning around 
+                your work and home life. You get to decide exactly when and where you study. ... Distance learning courses often cost less than full-time degrees, so you can save money.</p>
+            </div>
+            <div class="col-sm-12">
+              <div class="col-sm-12 mb-4 pt-lg-5">
+                <h1 data-aos="fade-up" data-aos-delay="100" class="text-white aos-init aos-animate">WHAT WE DO ? </h1>
+                <p class="mb-4 aos-init aos-animate text-white" data-aos="fade-up" data-aos-delay="200">MATRIX INSTITUTE OF INFORMATION TECHNOLOGY (PVT) LTD, located in the heart of Colombo, No.04, Bambalapitiya Drive, Galle Road, Colombo-04 offers the BCS - UK, Higher Education Qualification and is one of the leading institutes in Sri Lanka to conduct BCS and is an Approved Course Provider.
+                  BCS, Higher Education Qualifications (HEQ), leads to become a Chartered member of the society and further BCS is the only Chartered Body for IT in the world. BCS HEQ is equivalent to Bachelor’s Degree in Computing. Another main program offered by MATRIX is the Bachelor of Information Technology (BIT) of the University Of Colombo (UCSC), and has been ranked number 
+                  one in Institute analysis for several semesters for consecutive years. Institute has been able to maintain and further build the market share with the high quality teaching. The newest wing added to MATRIX is the Corporate Consultancy and Training arm which is mainly in HR training and Development.</p>
+              </div>                                                                                                                                                                                
+            </div> 
+          </div>
+        </div>
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
+						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+	</div>
+	</body>
+</html>
+
